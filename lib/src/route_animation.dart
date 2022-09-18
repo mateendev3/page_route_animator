8a8d95e7c5553b2f1ac92e7838378d2b38508deb
Mateen <mateendev3@gmail.com>
2022-09-18 14:43:16 +0500

    Documentized RouteAnimation enum

--- lib/src/route_animation.dart
+++ lib/src/route_animation.dart
@@ -1,64 +1,188 @@
+/// Route animation which you want to you to navigate.
 enum RouteAnimation {
+  /// Slide animation from top to bottom
   topToBottom,
+
+  /// Slide animation form bottom to top
   bottomToTop,
+
+  /// Slide animation from left to right
   leftToRight,
+
+  /// Slide animation form right to left
   rightToLeft,
+
+  /// Fade animation
   fade,
+
+  /// Rotate animation
   rotate,
+
+  /// Scale animation
   scale,
+
+  /// Size animation form center
   size,
+
+  /// Size animation form top
   sizeFromTop,
+
+  /// Size animation form bottom
   sizeFromBottom,
+
+  /// Fade and rotate animation
   fadeAndRotate,
+
+  /// Fade and scale animation
   fadeAndScale,
+
+  /// Rotate and scale animation
   rotateAndScale,
+
+  /// Fade, rotate and scale animation
   fadeRotateAndScale,
+
+  /// Slide animation form top to bottom as joined
   topToBottomJoined,
+
+  /// Slide animation form bottom to top as joined
   bottomToTopJoined,
+
+  /// Slide animation form left to right as joined
   leftToRightJoined,
+
+  /// Slide animation from right to left as joined
   rightToLeftJoined,
+
+  /// Slide animation form topLeft to bottomRight
   topLeftToBottomRight,
+
+  /// Slide animation from topRight to bottomLeft
   topRightToBottomLeft,
+
+  /// Slide animation form bottomLeft to topRight
   bottomLeftToTopRight,
+
+  /// Slide animation form bottomRight to topLeft
   bottomRightToTopLeft,
+
+  /// Slide animation form top to bottom with fading
   topToBottomWithFade,
+
+  /// Slide animation from bottom to top with fading
   bottomToTopWithFade,
+
+  /// Slide animation from left to right with fading
   leftToRightWithFade,
+
+  /// Slide animation from right to left with fading
   rightToLeftWithFade,
+
+  /// Slide animation from topLeft to bottomRight with fading
   topLeftToBottomRightWithFade,
+
+  /// Slide animation from bottomLeft to topRight with fading
   bottomLeftToTopRightWithFade,
+
+  /// Slide animation from bottomRight to topLeft with fading
   bottomRightToTopLeftWithFade,
+
+  /// Slide animation from topRight to bottomLeft with fading
   topRightToBottomLeftWithFade,
+
+  /// Slide animation from top to bottom with rotation
   topToBottomWithRotation,
+
+  /// Slide animation from bottom to top with rotation
   bottomToTopWithRotation,
+
+  /// Slide animation from left to right with rotation
   leftToRightWithRotation,
+
+  /// Slide animation from right to left with rotation
   rightToLeftWithRotation,
+
+  /// Slide animation from topLeft to bottomRight with rotation
   topLeftToBottomRightWithRotation,
+
+  /// Slide animation from bottomLeft to topRight with rotation
   bottomLeftToTopRightWithRotation,
+
+  /// Slide animation from bottomRight to topLeft with rotation
   bottomRightToTopLeftWithRotation,
+
+  /// Slide animation from topRight to bottomLeft with rotation
   topRightToBottomLeftWithRotation,
+
+  /// Slide animation from top to bottom with fade and rotate animation
   topToBottomWithFadeAndRotate,
+
+  /// Slide animation from bottom to top with fade and rotate animation
   bottomToTopWithFadeAndRotate,
+
+  /// Slide animation from left to right with fade and rotate animation
   leftToRightWithFadeAndRotate,
+
+  /// Slide animation from right to left with fade and rotate animation
   rightToLeftWithFadeAndRotate,
+
+  /// Slide animation from topLeft to bottomRight with fade and rotate animation
   topLeftToBottomRightWithFadeAndRotate,
+
+  /// Slide animation from topRight to bottomLeft with fade and rotate animation
   topRightToBottomLeftWithFadeAndRotate,
+
+  /// Slide animation from bottomLeft to topRight with fade and rotate animation
   bottomLeftToTopRightWithFadeAndRotate,
+
+  /// Slide animation from bottomRight to topLeft with fade and rotate animation
   bottomRightToTopLeftWithFadeAndRotate,
+
+  /// Slide animation from top to bottom with scaling
   topToBottomWithScale,
+
+  /// Slide animation from bottom to top with scaling
   bottomToTopWithScale,
+
+  /// Slide animation from left to right with scaling
   leftToRightWithScale,
+
+  /// Slide animation from right to left with scaling
   rightToLeftWithScale,
+
+  /// Slide animation from topLeft to bottomRight with scaling
   topLeftToBottomRightWithScale,
+
+  /// Slide animation from topRight to bottomLeft with scaling
   topRightToBottomLeftWithScale,
+
+  /// Slide animation from bottomLeft to topRight with scaling
   bottomLeftToTopRightWithScale,
+
+  /// Slide animation from bottomRight to topLeft with scaling
   bottomRightToTopLeftWithScale,
+
+  /// Slide animation from top to bottom with fade, rotate and scale animation
   topToBottomWithFadeRotateAndScale,
+
+  /// Slide animation from bottom to top with fade, rotate and scale animation
   bottomToTopWithFadeRotateAndScale,
+
+  /// Slide animation from left to right with fade, rotate and scale animation
   leftToRightWithFadeRotateAndScale,
+
+  /// Slide animation from right to left with fade, rotate and scale animation
   rightToLeftWithFadeRotateAndScale,
+
+  /// Slide animation from topLeft to bottomRight with fade, rotate and scale animation
   topLeftToBottomRightWithFadeRotateAndScale,
+
+  /// Slide animation from topRight to bottomLeft with fade, rotate and scale animation
   topRightToBottomLeftWithFadeRotateAndScale,
+
+  /// Slide animation from bottomLeft to topRight with fade, rotate and scale animation
   bottomLeftToTopRightWithFadeRotateAndScale,
+
+  /// Slide animation from bottomRight to topLeft with fade, rotate and scale animation
   bottomRightToTopLeftWithFadeRotateAndScale,
 }
